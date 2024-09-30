bf9b52cf95324130e3e91ef723a1548072998f1b
Nekitori17 <cuongnguyen286641@gmail.com>
2024-09-30 20:09:00 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Work in progress...</h1>
+</template>
+
+<style scoped>
+  h1 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
+<style module src="./assets/style.css"></style>
